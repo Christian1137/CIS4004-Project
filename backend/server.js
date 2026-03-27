eaccfdaf90bb5c5a7384e0ce9deb8579b167ec28
Christian1137 <chris.y5@icloud.com>
2026-03-26 23:00:37 -0400

    First Project Commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Server Running'));
+
+app.listen(PORT, () => console.log(`Server on port ${PORT}`));
+
+
+
+// Test Route
+app.get('/api/test', (req, res) => {
+  res.json({ message: "Hello from the backend!" });
+});
