bb30e58e983eceff32f92b139dd37a3c06766a87
Christian1137 <chris.y5@icloud.com>
2026-04-01 21:23:35 -0400

    Basic register/login routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const User = require('./models/User');
+
 require('dotenv').config();
 
 const app = express();
@@ -9,7 +11,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect('mongodb://localhost:27017/CIS4004ProjectDB')
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
@@ -23,3 +25,42 @@ app.listen(PORT, () => console.log(`Server on port ${PORT}`));
 app.get('/api/test', (req, res) => {
   res.json({ message: "Hello from the backend!" });
 });
+
+// Register Route
+app.post('/register', async (req, res) => {
+  try {
+    const { username, password, role } = req.body;
+
+    const newUser = new User({
+      username,
+      password,
+      role: role || 'Standard'
+    });
+
+    await newUser.save();
+    res.status(201).send("User created successfully!");
+  } catch (err) { 
+    res.status(400).send("Error: Username already exists.");
+  }
+})
+
+// Login Route 
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    return res.status(404).send("User not found");
+  }
+
+  if (user.password === password) { 
+    res.json({ 
+      message: "Login successful!", 
+      role: user.role,
+      username: user.username 
+    });
+  } else {
+    res.status(401).send("Incorrect password");
+  }
+})
